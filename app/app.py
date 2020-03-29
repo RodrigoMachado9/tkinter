95912dfd6d0c63f955bc2064ee2984031860467e
rmachado <rodrigo.machado3.14@hotmail.com>
2020-03-29 02:52:33 -0300

    in progress

--- app/app.py
+++ app/app.py
@@ -1,41 +1,43 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, Frame
+
+
+class Aplication(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.mount_application()
+        self.pack()
+
+    def lbl(self):
+        name_label = ttk.Label(root, text="Name: ")  # lbl name;
+        name_label.pack(side="left", padx=(0, 10))
+        name_entry = ttk.Entry(root, width=15, textvariable=self.user_name)  # lbl entry name;
+        name_entry.pack(side='left')
+        name_entry.focus()
+
+    def lbl_container_main(self):
+        # telinha....
+        ttk.Label(root, text='Telinha marota!', padding=(100, 80)).pack()  # name { container } tela
+
+    def btn(self):
+        greet_button = ttk.Button(root, text='Greet', command=self.greet, padding=(40, 20))
+        greet_button.pack(side='left', fill='y', expand=True)
+        quit_button = ttk.Button(root, text='Quit', command=root.destroy, padding=(40, 20))
+        quit_button.pack(side='left', fill='x', expand=True)
+
+    def mount_application(self):
+        self.user_name = tk.StringVar()
+        self.lbl_container_main()
+        self.lbl()
+        self.btn()
+
+    def greet(self):
+        print(f"Hello {self.user_name.get() or 'world'}!")
+
+
+
 
 root = tk.Tk()
-
-
-
-def greet():
-    print(f"Hello {user_name.get() or 'world'}!")
-
-
-def name():
-    print('rodrigo machado')
-
-user_name = tk.StringVar()
-
-
-name_label = ttk.Label(root, text="Name: ")                       # lbl name;
-name_label.pack(side="left", padx=(0, 10))
-name_entry = ttk.Entry(root, width=15, textvariable=user_name)     # lbl entry name;
-name_entry .pack(side='left')
-name_entry.focus()
-
-
-
-
-# telinha....
-ttk.Label(root, text='Telinha marota!', padding=(100, 80)).pack()                      # name { container } tela
-
-
-greet_button = ttk.Button(root, text='Greet', command=greet, padding=(40, 20))
-greet_button.pack(side='left', fill='y', expand=True)
-
-quit_button = ttk.Button(root, text='Quit', command=root.destroy, padding=(40, 20))
-quit_button.pack(side='left', fill='x', expand=True)
-
-root.mainloop()
-
-
-
-
+root.title('Welcome for application')
+app = Aplication(master=root)
+app.mainloop()
