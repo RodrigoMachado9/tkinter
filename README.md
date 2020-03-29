322c40d626e51dd0dce53538188dedef37f1da7e
rmachado <rodrigo.machado3.14@hotmail.com>
2020-03-29 01:10:00 -0300

    in progress

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tkinter
+
+##### Configuration tkinter.
+
+    - sudo  apt-get install python3-tk
+    - or 
+    - sudo sudo apt-get install python3-tk-dbg
--- app/app.py
+++ app/app.py
@@ -1,26 +1,35 @@
 import tkinter as tk
 from tkinter import ttk
 
-
-
 root = tk.Tk()
 
-# telinha...
-ttk.Label(root, text='Hello world!', padding=(100, 80)).pack()
 
 
 def greet():
-    print("Hello world")
+    print(f"Hello {user_name.get() or 'world'}!")
 
 
 def name():
     print('rodrigo machado')
 
+user_name = tk.StringVar()
+
+
+name_label = ttk.Label(root, text="Name: ")                     # lbl name;
+name_label.pack(side="left", padx=(0, 10))
+name_entry = ttk.Entry(root, width=15, textvariable=user_name)     # lbl entry name;
+name_entry .pack(side='left')
+name_entry.focus()
+
+# telinha....
+ttk.Label(root, text='Telinha marota!', padding=(100, 80)).pack()                      # name { container } tela
+
+
 greet_button = ttk.Button(root, text='Greet', command=greet, padding=(40, 20))
-greet_button.pack(side='left', fill='x')
+greet_button.pack(side='left', fill='y', expand=True)
 
 quit_button = ttk.Button(root, text='Quit', command=root.destroy, padding=(40, 20))
-quit_button.pack(side='right', fill='x')
+quit_button.pack(side='left', fill='x', expand=True)
 
 root.mainloop()
 
